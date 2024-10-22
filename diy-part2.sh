f7b51dbf4cb0a0ae961dbf39aa85d8219f11887a
Benjamin <benjaminx@gmail.com>
2024-10-22 12:14:18 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,10 +12,7 @@
 
 # Modify default IP
 echo '### Updates default IP gate ###'
-sed -i 's/192.168.1.1/10.1.1.10/g' package/base-files/files/bin/config_generate
-sed -i 's/addr_offset=2/addr_offset=20/g' package/base-files/files/bin/config_generate
-sed -i 's/192.168.$((addr_offset++)).1/10.1.1.$((addr_offset++))/g' package/base-files/files/bin/config_generate
-cat package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.1.1.10/g' package/base-files/luci2/bin/config_generate
 echo '###  ###'
 
 # echo '### Updates Theme Argon ###'
